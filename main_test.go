5eaeefde7eb4d6dcaf367d79e859a1af65abe447
maknahar <maknahar@live.in>
2018-03-25 18:05:40 +0530

    Added test case

--- main_test.go
+++ main_test.go
@@ -1,7 +1,24 @@
 package main
 
-import "testing"
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"testing"
+)
 
 func TestProcessIncomingEvents(t *testing.T) {
-	//err:=ProcessIncomingEvents()
+	err := ProcessIncomingEvents(events.S3Event{
+		Records: []events.S3EventRecord{{
+			S3: events.S3Entity{
+				Bucket: events.S3Bucket{
+					Name: "s3copyinput",
+				},
+				Object: events.S3Object{
+					Key: "config.json",
+				},
+			},
+		}},
+	})
+	if err != nil {
+		t.Fatal("Got Error ", err)
+	}
 }
