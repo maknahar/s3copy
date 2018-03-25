3df4a780c6f96ffe62a25e506d5a8fedaead0e7c
maknahar <maknahar@live.in>
2018-03-25 16:08:59 +0530

    Added Support for config via env var

--- main.go
+++ main.go
@@ -4,60 +4,73 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"sync"
 
+	"encoding/base64"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-func ProcessIncomingEvents(request events.S3Event) error {
-	// Get the data
-	resp, err := http.Get(os.Getenv("CONFIG_FILE"))
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
+func ParseConfig() (config map[string][]string, err error) {
+	data := make([]byte, 0)
+	configURL := os.Getenv("CONFIG_FILE")
+	if configURL == "" {
+		// Get the data
+		resp, err := http.Get(configURL)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
 
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
+		data, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		data, err = base64.StdEncoding.DecodeString(os.Getenv("CONFIG"))
+		if err != nil {
+			return nil, err
+		}
 	}
-	config := make(map[string][]string)
+
+	if len(data) == 0 {
+		return nil, fmt.Errorf("no configuration available")
+	}
+
+	config = make(map[string][]string)
 
 	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func ProcessIncomingEvents(request events.S3Event) error {
+	config, err := ParseConfig()
 	if err != nil {
 		return err
 	}
 
 	var wg sync.WaitGroup
 	for _, v := range request.Records {
-		sess := session.Must(session.NewSession())
-		region, err := s3manager.GetBucketRegion(nil, sess, v.S3.Bucket.Name, "ap-southeast-1")
+		region, err := s3manager.GetBucketRegion(nil, session.Must(session.NewSession()), v.S3.Bucket.Name, "ap-southeast-1")
 		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok && aerr.Code() == "NotFound" {
-				fmt.Fprintf(os.Stderr, "unable to find bucket %s's region not found\n", v.S3.Bucket.Name)
-			}
 			return err
 		}
 
-		sess, err = session.NewSession(&aws.Config{
-			Region: aws.String(region)},
-		)
-		svc := s3.New(sess)
-
+		sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
 		for _, v1 := range config[v.S3.Bucket.Name] {
 			wg.Add(1)
-			go copyObjects(&wg, svc, v.S3.Bucket.Name, v1, v.S3.Object.Key)
+			go copyObjects(&wg, s3.New(sess), v.S3.Bucket.Name, v1, v.S3.Object.Key)
 		}
-
 	}
 	wg.Wait()
 	return nil
@@ -71,18 +84,15 @@ func copyObjects(wg *sync.WaitGroup, svc *s3.S3, from, to, item string) error {
 		return fmt.Errorf("unable to copy item %s from bucket %q to bucket %q, %v", item, from, to, err)
 	}
 
-	// Wait to see if the item got copied
 	err = svc.WaitUntilObjectExists(&s3.HeadObjectInput{Bucket: aws.String(to), Key: aws.String(item)})
 	if err != nil {
 		return fmt.Errorf("error occurred while waiting for item %q to be copied to bucket %q, %v",
 			item, to, err)
 	}
 
-	log.Printf("Item %q successfully copied from bucket %q to bucket %q\n", item, from, to)
 	return nil
 }
 
 func main() {
 	lambda.Start(ProcessIncomingEvents)
-	//log.Println(ProcessIncomingEvents())
 }
